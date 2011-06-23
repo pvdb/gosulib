544fd6fbc843bf5552e4f9e59a94dba368a964e5
Peter Vandenberk <pvandenberk@mac.com>
2011-06-23 23:24:46 +0100

    points can now be extended to be moving

---
+++ lib/point/moving.rb
@@ -0,0 +1,22 @@
+module Moving
+
+  def self.extended(object)
+    object.instance_variable_set(:@horizontal_speed, 0)
+    object.instance_variable_set(:@vertical_speed, 0)
+  end
+
+  def horizontal_speed= horizontal_speed
+    @horizontal_speed = horizontal_speed
+  end
+
+  def vertical_speed= vertical_speed
+    @vertical_speed = vertical_speed
+  end
+
+  def update
+    super
+    @x += @horizontal_speed
+    @y += @vertical_speed
+  end
+  
+end
--- main.rb
+++ main.rb
@@ -8,12 +8,15 @@ Dir.glob('**/*.rb').each do |file|
   require file.gsub(/\.rb/, '')
 end
 
+speeds = (-5..5).to_a
+
 game = Game.new(640, 480, false)
 
 1.upto(1024) do
   point = game.add_point(rand(game.width), rand(game.height))
-  point.extend(Disappearing)
-  point.lifetime = rand(300)
+  point.extend(Moving)
+  point.horizontal_speed = speeds[rand(speeds.size)]
+  point.vertical_speed = speeds[rand(speeds.size)]
 end
 
 game.show
