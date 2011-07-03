5fe86196490cae1a2e39fb5c702a4b5879f24422
Peter Vandenberk <pvandenberk@mac.com>
2011-07-03 20:21:34 +0100

    Game refactoring
    The Game class has been refactored into a pretty much abstract superclass, and two concrete subclasses: PointGame and LineGame

--- game.rb
+++ game.rb
@@ -2,13 +2,50 @@ class Game < Gosu::Window
 
   def initialize(width, height, fullscreen)
     super(width, height, fullscreen)
-    @points = [] ; @lines = []
     @text = Gosu::Font.new(self, 'Monaco', 24)
+  end
+
+  def button_down(id)
+    self.close if id == Gosu::KbEscape
+  end
+  
+  def draw
+    @text.draw("Press 'esc' to end the game...", 10, 10, 0)
+  end
+
+end
+
+class PointGame < Game
+  
+  def initialize(width, height, fullscreen)
+    super(width, height, fullscreen)
+    @points = []
   end
 
   def add_point(x, y, colour = Gosu::Color::WHITE)
     @points << (point = Point.new(x, y, colour))
     return point
+  end
+  
+  def update
+    self.close if @points.empty?
+
+    @points = @points.find_all { |point| !point.purge? self }
+    @points.each { |point| point.update self  }
+  end
+
+  def draw
+    @text.draw("Points: " + @points.size.to_s, 10, 10, 0)
+    @points.each { |point| point.draw(self)  }
+  end
+  
+end
+
+class LineGame < Game
+  
+  def initialize(width, height, fullscreen)
+    super(width, height, fullscreen)
+    @lines = []
   end
 
   def add_line(start_point, end_point)
@@ -17,25 +54,15 @@ class Game < Gosu::Window
   end
 
   def update
-    self.close if @points.empty? && @lines.empty?
-
-    @points = @points.find_all { |point| !point.purge? self }
-    @points.each { |point| point.update self  }
+    self.close if @lines.empty?
 
     @lines = @lines.find_all { |line| !line.purge? self }
     @lines.each { |line| line.update self  }
   end
 
   def draw
-    @text.draw("Points: " + @points.size.to_s, 10, 10, 0)
-    @points.each { |point| point.draw(self)  }
-
-    @text.draw("Lines: " + @lines.size.to_s, 10, 40, 0)
+    @text.draw("Lines: " + @lines.size.to_s, 10, 10, 0)
     @lines.each { |line| line.draw(self)  }
   end
-
-  def button_down(id)
-    self.close if id == Gosu::KbEscape
-  end
-
+  
 end
--- main.rb
+++ main.rb
@@ -8,52 +8,5 @@ Dir.glob('**/*.rb').each do |file|
   require file.gsub(/\.rb/, '')
 end
 
-
 game = Game.new(640, 480, false)
-
-# 1.upto(128) do
-#   start_point = Point.new(rand(game.width), rand(game.height), Colour.random)
-#     start_point.extend(Moving)
-#     start_point.extend(Disappearing)
-#     start_point.lifetime = rand(300)
-#     start_point.horizontal_speed = Speed.random
-#     start_point.vertical_speed = Speed.random
-#   end_point = Point.new(rand(game.width), rand(game.height), Colour.random)
-#     end_point.extend(Moving)
-#     end_point.extend(Disappearing)
-#     end_point.lifetime = rand(300)
-#     end_point.horizontal_speed = Speed.random
-#     end_point.vertical_speed = Speed.random
-#   game.add_line start_point, end_point
-# end
-# 1.upto(1024) do
-#   point = game.add_point(rand(game.width), rand(game.height))
-#   point.extend(Disappearing)
-#   point.lifetime = rand(300)
-# end
-# 1.upto(1024) do
-#   point = game.add_point(rand(game.width), rand(game.height))
-#   point.extend(Moving)
-#   point.horizontal_speed = Speed.random
-#   point.vertical_speed = Speed.random
-# end
-# 1.upto(1024) do
-#   point = game.add_point(rand(game.width), rand(game.height), Colour.random)
-#   point.extend(Moving)
-#   point.horizontal_speed = Speed.random
-#   point.vertical_speed = Speed.random
-# end
-1.upto(1024) do
-  point = game.add_point(game.width / 2, game.height / 2, Colour.random)
-  point.extend(Moving)
-  point.horizontal_speed = Speed.random(-10, 10)
-  point.vertical_speed = Speed.random(-10, 10)
-end
-# 1.upto(1024) do
-#   point = game.add_point(rand(game.width), rand(game.height), Gosu::Color::GREEN)
-#   point.extend(Moving)
-#   # point.horizontal_speed = Speed.random
-#   point.vertical_speed = Speed.random
-# end
-
 game.show
