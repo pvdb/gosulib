dfc7def4a09972bb5747645e8504701bb0ea0805
Peter Vandenberk <pvandenberk@mac.com>
2011-07-06 20:24:43 +0100

    Bees are yellow, dude!

--- games/bee_swarm.rb
+++ games/bee_swarm.rb
@@ -1,7 +1,7 @@
 class MovingPoints < PointGame
   def initialize(width, height, fullscreen)
     super(width, height, fullscreen, 1024) do |game|
-      point = game.add_point(rand(game.width), rand(game.height))
+      point = game.add_point(rand(game.width), rand(game.height), Gosu::Color::YELLOW)
       point.extend(Moving)
       point.horizontal_speed = Speed.random(-10, 10)
       point.vertical_speed = Speed.random(-10, 10)
