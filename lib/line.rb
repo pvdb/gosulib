3f84f3d378322fa967503b9aab4ca1aedad87dac
Peter Vandenberk <pvandenberk@mac.com>
2011-07-08 09:18:12 +0100

    attr accessors for start and end points, as well as length calculation

--- lib/line.rb
+++ lib/line.rb
@@ -1,10 +1,16 @@
 class Line
-  
+
+  attr_accessor :start_point, :end_point
+
   def initialize(start_point, end_point)
     @start_point = start_point
     @end_point = end_point
   end
-  
+
+  def length
+    Gosu.distance(@start_point.x, @start_point.y, @end_point.x, @end_point.y)
+  end
+
   def purge? window
     @start_point.purge?(window) && @end_point.purge?(window)
   end
@@ -13,9 +19,9 @@ class Line
     @start_point.update window
     @end_point.update window
   end
-  
+
   def draw window
     window.draw_line(@start_point.x, @start_point.y, @start_point.colour, @end_point.x, @end_point.y, @end_point.colour)
   end
-  
+
 end
