a503437a29d2a7b632ffdbc85dfd2a2a430bacb3
Peter Vandenberk <pvandenberk@mac.com>
2011-07-08 09:36:23 +0100

    snow gets accumulated on the ground

--- games/falling_snow.rb
+++ games/falling_snow.rb
@@ -1,12 +1,36 @@
 class FallingSnow < PointGame
+
   def initialize(width, height, fullscreen)
-    super(width, height, fullscreen, width / 16) do |game|
-      point = game.add_point(rand(width), rand(height) - height)
+
+    super(width, height, fullscreen, width / 8) do |game|
+      point = game.add_point(rand(width), 0)
       point.extend(Moving)
       # point.horizontal_speed = Speed.random(-5, 5)
-      point.vertical_speed = 2 + Speed.random(-1, 1, false)
+      point.vertical_speed = 2 + Speed.random(-1, 1)
     end
+
+    @lines = (0..width - 1).map { |x|
+      Line.new(Point.new(x, height - 1), Point.new(x, height - 1))
+    }
+
   end
+
+  def update
+    @points.each do |point|
+      line = @lines[point.x]
+      if point.next_y >= line.start_point.y
+        point.y = height # so it gets purged
+        line.start_point.y -= 1
+      end
+    end
+    super
+  end
+
+  def draw
+    super
+    @lines.map { |line| line.draw self }
+  end
+
 end
 
 $GAME_CLASS = FallingSnow
